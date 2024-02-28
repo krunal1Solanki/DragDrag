5223f1bace13aadfb2c1d7ddf0edd31530856dde
Delta <delta@Deltas-MacBook-Pro.local>
2024-02-28 10:56:53 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
+import React from 'react';
+import { ChakraProvider, Flex, Box } from '@chakra-ui/react';
+import { Resizable } from 'react-resizable';
+import 'react-resizable/css/styles.css';
+
+const ResizableBox = ({ width, height, onResize, children }) => (
+  <Resizable
+    width={width}
+    height={height}
+    onResize={onResize}
+    draggableOpts={{ enableUserSelectHack: false }}
+    resizeHandles={['s', 'w', 'e', 'n', 'sw', 'nw', 'se', 'ne']} // Enable all 8 handles
+  >
+    <Box
+      width="100%"
+      height="100%"
+      border="1px solid #ddd"
+      position="relative"
+      overflow="hidden"
+      resize="both"
+    >
+      {children}
+    </Box>
+  </Resizable>
+);
+
+const App = () => {
+  const handleResize = (event, { size }) => {
+    // Handle resizing logic here
+    console.log(`Resized to width: ${size.width}, height: ${size.height}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Flex height="100vh" direction="row">
+        <ResizableBox width={200} height={400} onResize={handleResize}>
+          {/* Component content */}
+          <p>Component 1</p>
+        </ResizableBox>
+        <ResizableBox width={200} height={400} onResize={handleResize}>
+          {/* Component content */}
+          <p>Component 2</p>
+        </ResizableBox>
+        <ResizableBox width={200} height={400} onResize={handleResize}>
+          {/* Component content */}
+          <p>Component 3</p>
+        </ResizableBox>
+      </Flex>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
